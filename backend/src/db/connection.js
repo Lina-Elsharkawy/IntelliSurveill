5dc4fe8535c246aa21609666569d86fc8daf8651
mohamedmostafam0 <127888398+mohamedmostafam0@users.noreply.github.com>
2025-12-02 23:35:17 +0200

    refactor: use env vars for initial PostgreSQL database connection using Sequelize

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,13 +1,14 @@
 const { Sequelize } = require('sequelize');
 const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../logging.env') });
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const {
-  POSTGRES_USER = 'lina',
-  POSTGRES_PASSWORD = '123',
-  POSTGRES_DB = 'linadb',
-  POSTGRES_HOST = 'localhost',
-  POSTGRES_PORT = 5432,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
 } = process.env;
 
 const sequelize = new Sequelize(
