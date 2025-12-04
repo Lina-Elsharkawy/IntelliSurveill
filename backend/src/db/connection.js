36cb94cd059d3ff9c8513373b9598fb4aa64ce4e
mohamedmostafam0 <127888398+mohamedmostafam0@users.noreply.github.com>
2025-12-04 15:21:34 +0200

    feat: add .env.examples, error handling to connection.js file

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -11,6 +11,11 @@ const {
   POSTGRES_PORT,
 } = process.env;
 
+if (!POSTGRES_USER || !POSTGRES_PASSWORD || !POSTGRES_DB) {
+  console.error('Missing required environment variables for database connection.');
+  process.exit(1);
+}
+
 const sequelize = new Sequelize(
   `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`,
   { dialect: 'postgres', logging: false }
