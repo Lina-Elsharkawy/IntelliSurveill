b673fb13b1954fda3da25ae256e44602d3fc839a
Malak-Abdelakher <21P0278@eng.asu.edu.eg>
2025-12-10 13:59:44 +0200

    update

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -7,14 +7,14 @@ router.post('/login', async (req, res) => {
 
     try {
         const response = await axios.post(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, {
-            grant_type: 'password',
+            grant_type: 'http://auth0.com/oauth/grant-type/password-realm',
             username,
             password,
             audience: process.env.AUTH0_AUDIENCE,
             client_id: process.env.AUTH0_CLIENT_ID,
             client_secret: process.env.AUTH0_CLIENT_SECRET,
             realm: 'Username-Password-Authentication',
-            scope: 'openid profile email'
+            scope: 'openid profile email offline_access'
         }, {
             headers: { 'Content-Type': 'application/json' }
         });
@@ -27,5 +27,4 @@ router.post('/login', async (req, res) => {
         });
     }
 });
-
 module.exports = router;
